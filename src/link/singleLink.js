3ece5a0ae45e9db3a36ee399814facf97b8b8331
ChrisWen <957638221@qq.com>
2018-09-05 14:28:08 +0800

    Add insertNode && printLink for SingleLink

--- src/link/singleLink.js
+++ src/link/singleLink.js
@@ -1,1 +1,24 @@
+class SingleLink {
+  constructor(singleLinkeNode) {
+    this.head = singleLinkeNode;
+    this.head.next = null;
+  }
 
+  insertNode(singleLinkeNode) {
+    const newNode = singleLinkeNode;
+    let currentNode = this.head;
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = newNode;
+  }
+
+  printLink() {
+    const currentNode = this.head;
+    while (currentNode.value !== null) {
+      console.log(currentNode.value, '=>');
+    }
+  }
+}
+
+module.exports = SingleLink;
