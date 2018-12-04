5af5e53008d56ba1ea6a25aed0777d015277b4f5
ChrisWen <957638221@qq.com>
2018-12-04 17:23:06 +0800

    Add async test

---
+++ src/async/index.js
@@ -0,0 +1,8 @@
+
+setTimeout(() => console.log('Win3'), 200);
+
+setTimeout(() => console.log('Win'), 0);
+
+setTimeout(() => console.log('Win2'), 100);
+
+Promise.resolve('Hello world').then(str => console.log(str));
