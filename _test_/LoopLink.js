197ca1e17c3b374b0bfda2336569d2cef7877881
ChrisWen <957638221@qq.com>
2019-03-14 15:53:45 +0800

    Init project

--- _test_/LoopLink.js
+++
@@ -1,8 +0,0 @@
-const LoopLink = require('../src/link/loopLink');
-
-const loopLink = new LoopLink(1);
-
-loopLink.insert({ value: 2, next: null });
-
-console.log(loopLink.getLinkLength());
-console.log(loopLink.find(2));
--- src/async/index.js
+++
@@ -1,8 +0,0 @@
-
-setTimeout(() => console.log('Win3'), 200);
-
-setTimeout(() => console.log('Win'), 0);
-
-setTimeout(() => console.log('Win2'), 100);
-
-Promise.resolve('Hello world').then(str => console.log(str));
